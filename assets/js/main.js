af4c169f7bb78ff62a22fe07f1c326585821a2fd
Solid Snake <Solid Snake>
2016-04-06 22:08:46 +0800

    cleaning up

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,11 @@ jQuery(document).ready(function($) {
 
 
     /*======= Skillset *=======*/
-    
+
 
     $('.level-bar-inner').css('width', '0');
 
     $(window).on('load', function() {
-
         $('.level-bar-inner').each(function() {
 
             var itemWidth = $(this).data('level');
